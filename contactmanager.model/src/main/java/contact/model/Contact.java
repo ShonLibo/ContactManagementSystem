b16a6b266fc863066af1876e75710a4da2a4b044
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-07 09:15:31 +0300

    #Constructor
    On line 8 to 12 a constructor for class Contact was added.

--- contactmanager.model/src/main/java/contact/model/Contact.java
+++ contactmanager.model/src/main/java/contact/model/Contact.java
@@ -5,4 +5,9 @@ public class Contact {
     private String phone;
     private String email;
 
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
 }
