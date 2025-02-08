1bb6fb4974caa1fbf5f750bacecc132f5e744469
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-08 21:38:31 +0300

    #integration test

--- contactmanager.test/src/test/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module contactmanager.test {
-    requires contactmanager.model;
-    requires contactmanager.ui;
-    requires contactmanager.app;
-    requires org.junit.jupiter.api;
-    requires java.desktop;
-    exports contactmanager.test;
-}
