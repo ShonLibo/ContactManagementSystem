ae835870f54352d1a1535ca2acbf41176092dd68
Onyango Samson Ochira Obur <onyangosamsonobur@gmail.com>
2025-02-09 20:02:12 +0300

    Update IT.java

--- contactmanager.test/src/test/java/integrationTest/IT.java
+++ contactmanager.test/src/test/java/integrationTest/IT.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class IntegrationTest {
+public class IT {
 
     private ContactManager contactManager;
 
